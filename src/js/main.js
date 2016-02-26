409ba17fb4d4a0c8701757cc2adbde5512bbbfb5
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-02-26 14:10:35 +0100

    A bit more

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 (function() {
   var form = document.getElementById('main-form'),
-      input = form.querySelector('input');
+      input = form.querySelector('input'),
+      result_bar = document.querySelector('.progress-bar');
 
   console.log("form: " + form);
 
@@ -8,7 +9,7 @@
     var kilometers = parseInt(input.value, 10);
 
     console.log("KM: " + kilometers);
-
+    result_bar.style.width = "50%";
     e.preventDefault();
   }, false);
 }());
